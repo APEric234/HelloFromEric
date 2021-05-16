9df75afb5edada22d6e5c3820e1e408e04168af8
swalloich <nels.jac42@gmail.com>
2021-05-15 21:11:20 -0600

    Added more to the project.

--- src/Allen/main.java
+++ src/Allen/main.java
@@ -3,5 +3,6 @@ package Allen;
 public class main {
     public static void Main(String[] args){
         System.out.println("Hello my name is Eric");
+        System.out.println("Wow! Github is cool!");
     }
 }
